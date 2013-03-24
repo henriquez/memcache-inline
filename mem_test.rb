0d1dbfb1573f7c1e7763ca34f25a88dd8421b25f
Logan Henriquez <logan@henriquez.net>
2013-03-24 16:35:00 -0700

    test

--- mem_test.rb
+++ mem_test.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'dalli'
 
-# make a bunch of requests to memcached on every get
+# make a bunch of requests to memcached on every get,
 get '/' do
 
   erb :index # render views/index.erb
