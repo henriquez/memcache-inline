832aa7b8bfa614b71af42e59a7b04bbb4bc0f1fd
Logan Henriquez <logan@henriquez.net>
2013-03-24 23:39:40 -0700

    removed old code

--- mem_test.rb
+++ mem_test.rb
@@ -37,21 +37,6 @@ get '/onekey/:queries' do
 end
 
 
-# Do equal number of sets and gets with small values
-get '/a/:requests' do
-  dc = Dalli::Client.new
-  @start_time = Time.now
-  @queries = params[:requests].to_i / 2 # get and set operation per iteration
-  @queries.to_i.times do |key|
-    dc.set(key, 'bar')
-    @out = dc.get(key)
-  end
-  @end_time = Time.now
-  @elapsed = @end_time - @start_time
-  @queries = params[:requests]
-  erb :index # render views/index.erb
-end
-
 
 def call_memcache(iterations, query)
   @start_time = Time.now
