9ad1be6afa7bb37fe82b1c65379f0d1112d9296b
Logan Henriquez <logan@henriquez.net>
2013-03-24 21:23:51 -0700

    test a done

--- mem_test.rb
+++ mem_test.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'dalli'
 
-# make a bunch of requests to memcached on every get,
-get '/' do
 
+
+# Do equal number of sets and gets with small values
+get '/a/:requests' do
+  dc = Dalli::Client.new('localhost:11211')
+  @start_time = Time.now
+  @queries = params[:requests].to_i / 2 # get and set operation per iteration
+  @queries.to_i.times do |key|
+    dc.set('foo', 'bar')
+    @out = dc.get('foo')
+  end
+  @end_time = Time.now
+  @elapsed = @end_time - @start_time
+  @queries = params[:requests]
   erb :index # render views/index.erb
 end
+
